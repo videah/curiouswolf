64449c1d774339fb0d04b7bbf0d381f136a54223
videah <videah@selfish.systems>
2023-01-23 09:19:13 +0000

    add new authentication ui to sign in pa

--- static/authentication.js
+++ static/authentication.js
@@ -1,10 +1,19 @@
 window.onload = function() {
     const { startAuthentication } = SimpleWebAuthnBrowser;
 
-    const button = document.getElementById("login-button");
+    const button = document.getElementById("sign-in-button");
     const error = document.getElementById("error-label");
 
-    async function attemptRegistration() {
+    let redirecting = false;
+
+    async function displayError(e) {
+        console.log(e);
+        button.classList.remove("loading");
+        error.innerText = await e.text();
+    }
+
+    async function attemptAuthentication() {
+        error.innerText = "";
         button.classList.add("loading");
 
         const username = document.getElementById("username");
@@ -15,43 +24,45 @@ window.onload = function() {
             return;
         }
 
-        const resp = await fetch("/auth/authenticate_start/" + username.value, { method: "POST" });
-        if (resp.status !== 200) {
-            error.innerText = "Error: Attempt to sign in failed";
-            button.classList.remove("loading");
-            return;
-        }
-
-        let asseResp;
-        try {
-            const decoded = await resp.json();
-            asseResp = await startAuthentication(decoded["publicKey"]);
-        } catch (e) {
-            button.classList.remove("loading");
-            console.log(e.name);
-            if (e.name === "NotAllowedError") {
-                error.innerText = "Error: Attempt to sign in was either cancelled or timed out";
-            } else {
-                error.innerText = e;
+        const resp = await fetch("/auth/authenticate_start/" + username.value, {method: "POST"});
+        if (resp.status === 200) {
+            let attResp;
+            try {
+                const decoded = await resp.json()
+                attResp = await startAuthentication(decoded["publicKey"]);
+            } catch (e) {
+                return displayError(e);
             }
-            error.innerText = e;
-            return;
-        }
 
-        const url = "/auth/authenticate_finish";
-        const verificationResp = await fetch(url, {
-            method: "POST",
-            headers: {
-                "Content-Type": "application/json"
-            },
-            body: JSON.stringify(asseResp),
-        });
+            const url = "/auth/authenticate_finish";
+            const verificationResp = await fetch(url, {
+                method: "POST",
+                headers: {
+                    "Content-Type": "application/json"
+                },
+                body: JSON.stringify(attResp),
+            });
 
-        if (verificationResp.status === 200) {
-            document.location.href = "/";
+            if (verificationResp.status === 200) {
+                const success = document.getElementById("button-success");
+                const label = document.getElementById("button-label");
+
+                button.classList.remove("~info");
+                button.classList.remove("loading");
+                button.classList.add("~positive");
+
+                label.classList.add("hidden");
+                success.classList.remove("hidden");
+
+                await new Promise(r => setTimeout(r, 300));
+                document.location.href = "/";
+            } else {
+                return displayError(verificationResp);
+            }
         } else {
-            error.innerText = "Error: Attempt to sign in failed";
+            return displayError(resp);
         }
     }
-    button.addEventListener("click", attemptRegistration);
+
+    button.addEventListener("click", () => {if (!redirecting) { attemptAuthentication() }});
 }
